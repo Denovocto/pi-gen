c087c1d49f4ab1f8d0f072455dcd2ff909d354f0
Jezreel J. Maldonado Ruiz <jezreel.maldonado@upr.edu>
2020-07-12 21:35:53 -0400

    Fixed typo's and errors in shell scripts on stage3

--- stage3/00-install-packages/03-run.sh
+++ stage3/00-install-packages/03-run.sh
@@ -1,4 +1,4 @@
 npm install sqlite3
 npm install
-curl --fsSL https://install.raspap.com | bash -y
+curl -fsSL https://install.raspap.com | bash -y
 git -C ~/ clone https://github.com/Project-Owl/dms-lite.git
